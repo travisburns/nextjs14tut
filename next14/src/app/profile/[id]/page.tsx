81f822b92f2e33ce5b26f3cdef922905969c0834
Travis <travisjb0@gmail.com>
2023-12-02 02:50:00 -0800

    added params to postid

--- next14/src/app/profile/[id]/page.tsx
+++ next14/src/app/profile/[id]/page.tsx
@@ -3,6 +3,6 @@ import {useParams} from 'next/navigation'
 
 export default function ProfileID() {
     const params = useParams();
-    console.log(params)
+    console.log(params.id)
     return <div>My profile id</div>
 }
--- next14/src/app/profile/[id]/post/[postId]/page.tsx
+++ next14/src/app/profile/[id]/post/[postId]/page.tsx
@@ -1,3 +1,9 @@
+'usse client';
+import { useParams } from "next/navigation";
+
+
 export default function ProfileIDPostID() {
+    const params = useParams();
+    console.log(params.id)
     return <div>My profile id</div>
 }
