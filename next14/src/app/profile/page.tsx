fd95c64d5d8b693cab4db0b6817a9c494d290b84
Travis <travisjb0@gmail.com>
2023-12-02 22:38:07 -0800

    the most rediculous error, userouter not mounted

--- next14/src/app/profile/page.tsx
+++ next14/src/app/profile/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useRouter } from "next/router"
+'use client'
+import { useRouter } from "next/navigation"
 
 export default function Profile() {
     const router = useRouter();
