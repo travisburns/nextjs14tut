c1908fe3d8221e46c128bc1d85d69b991be1cb65
Travis <travisjb0@gmail.com>
2023-12-03 15:19:56 -0800

    Link in Nextjs learned

--- next14/src/app/profile/page.tsx
+++ next14/src/app/profile/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+import Link from "next/link"
+
 import { useRouter } from "next/navigation"
 
 export default function Profile() {
@@ -11,11 +14,29 @@ export default function Profile() {
         
         <h1>my list of profiles</h1>
         <div>
-            <ul>
-                <li onClick={() => router.push('/profile/1')}>Profile 1</li>
-                <li onClick={() => router.push('/profile/2')}>Profile 2</li>
-                <li onClick={() => router.push('/profile/3')}>Profile 3</li>
-            </ul>
+           
+                <Link href='/profile/1'>Profile 1</Link>
+                <Link href='/profile/2'> Profile 2</Link>
+                <Link 
+                    prefetch={true}
+                    href={{
+                    pathname: '/profile/3',
+                    query: {
+                        userName: "Guillaume",
+                        userId: 12,
+                        person: JSON.stringify({
+                            age: 18,
+                        })
+                    },
+                }}
+                >
+                    <div>A big square where I got some text</div>
+                </Link>
+
+
+                <a href="https://codewithguillaume.com">
+                    CodeWithGuillaume's school online
+                    </a>
         </div>
         </div>
     )
